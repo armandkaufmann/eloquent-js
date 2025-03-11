fc7d7e4d16a6f14fdccf73e7a7c1fde5c806a099
Armand Kaufmann <armandkaufmann@gmail.com>
2025-03-10 21:55:14 -0400

    added statement whereIn and orWhereIn

---
+++ tests/builder/statement/where/OrWhereIn.spec.js
@@ -0,0 +1,50 @@
+import {describe, expect, test} from 'vitest';
+import OrWhereIn from "../../../../src/builder/statement/where/OrWhereIn.js";
+
+describe('Statement: OrWhereIn', () => {
+    describe('toString', () => {
+       test("It builds where partial statement", () => {
+           const column = 'names';
+           const values = ['John', 'Armand', 'Alex', 'Ian'];
+           const expectedResult = "names IN ('John', 'Armand', 'Alex', 'Ian')";
+
+           const result = new OrWhereIn(column, values).toString();
+
+           expect(result).toEqual(expectedResult);
+       });
+
+        test("It builds with 'AND' when withSeparator is true", () => {
+            const column = 'names';
+            const values = ['John', 'Armand', 'Alex', 'Ian'];
+            const expectedResult = "OR names IN ('John', 'Armand', 'Alex', 'Ian')";
+
+            const result = new OrWhereIn(column, values).toString(true);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('serialize', () => {
+        test('It builds a prepare object with the correct values', () => {
+            const column = 'names';
+            const values = ['John', 'Armand', 'Alex', 'Ian'];
+            const expectedQuery = "names IN (?, ?, ?, ?)";
+
+            const result = new OrWhereIn(column, values).serialize();
+
+            expect(result.query).toEqual(expectedQuery);
+            expect(result.bindings).toEqual(values);
+        });
+
+        test('It builds a prepare object query with "AND"', () => {
+            const column = 'names';
+            const values = ['John', 'Armand', 'Alex', 'Ian'];
+            const expectedQuery = "OR names IN (?, ?, ?, ?)";
+
+            const result = new OrWhereIn(column, values).serialize(true);
+
+            expect(result.query).toEqual(expectedQuery);
+            expect(result.bindings).toEqual(values);
+        });
+    });
+});
---
+++ tests/builder/statement/where/WhereIn.spec.js
@@ -0,0 +1,50 @@
+import {describe, expect, test} from 'vitest';
+import WhereIn from "../../../../src/builder/statement/where/WhereIn.js";
+
+describe('Statement: WhereIn', () => {
+    describe('toString', () => {
+       test("It builds where partial statement", () => {
+           const column = 'names';
+           const values = ['John', 'Armand', 'Alex', 'Ian'];
+           const expectedResult = "names IN ('John', 'Armand', 'Alex', 'Ian')";
+
+           const result = new WhereIn(column, values).toString();
+
+           expect(result).toEqual(expectedResult);
+       });
+
+        test("It builds with 'AND' when withSeparator is true", () => {
+            const column = 'names';
+            const values = ['John', 'Armand', 'Alex', 'Ian'];
+            const expectedResult = "AND names IN ('John', 'Armand', 'Alex', 'Ian')";
+
+            const result = new WhereIn(column, values).toString(true);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('serialize', () => {
+        test('It builds a prepare object with the correct values', () => {
+            const column = 'names';
+            const values = ['John', 'Armand', 'Alex', 'Ian'];
+            const expectedQuery = "names IN (?, ?, ?, ?)";
+
+            const result = new WhereIn(column, values).serialize();
+
+            expect(result.query).toEqual(expectedQuery);
+            expect(result.bindings).toEqual(values);
+        });
+
+        test('It builds a prepare object query with "AND"', () => {
+            const column = 'names';
+            const values = ['John', 'Armand', 'Alex', 'Ian'];
+            const expectedQuery = "AND names IN (?, ?, ?, ?)";
+
+            const result = new WhereIn(column, values).serialize(true);
+
+            expect(result.query).toEqual(expectedQuery);
+            expect(result.bindings).toEqual(values);
+        });
+    });
+});
