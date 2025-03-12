0755d868af06edfe477eb3d7969c60daa52acfd4
Armand Kaufmann <armandkaufmann@gmail.com>
2025-03-12 09:53:52 -0400

    added WhereBetween statement

---
+++ tests/builder/statement/where/WhereBetween.spec.js
@@ -0,0 +1,26 @@
+import {describe, expect, test} from 'vitest';
+import WhereBetween from "../../../../src/builder/statement/where/WhereBetween.js";
+
+describe('Statement: WhereBetween', () => {
+    describe('toString', () => {
+        test("It builds where partial statement", () => {
+            const column = 'age';
+            const values = [20, 55];
+            const expectedResult = "age BETWEEN 20 and 55";
+
+            const result = new WhereBetween(column, values).toString();
+
+            expect(result).toEqual(expectedResult);
+        });
+
+        test("It builds with 'AND' when withSeparator is true", () => {
+            const column = 'age';
+            const values = [20, 55];
+            const expectedResult = "AND age BETWEEN 20 and 55";
+
+            const result = new WhereBetween(column, values).toString(true);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+});
