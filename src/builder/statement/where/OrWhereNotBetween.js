1c319ead16fefdcd897606ce1d4e348f9b18a4ea
Armand Kaufmann <armandkaufmann@gmail.com>
2025-03-12 22:59:34 -0400

    added statement OrWhereNotBetween

---
+++ src/builder/statement/where/OrWhereNotBetween.js
@@ -0,0 +1,13 @@
+import WhereBetween from "./WhereBetween.js";
+import WhereNotBetween from "./WhereNotBetween.js";
+
+export default class OrWhereNotBetween extends WhereNotBetween {
+
+    /**
+     * @param {String} column
+     * @param {Array<number>} values
+     */
+    constructor(column, values) {
+        super(column, values, 'OR');
+    }
+}
---
+++ tests/builder/statement/where/OrWhereNotBetween.spec.js
@@ -0,0 +1,26 @@
+import {describe, expect, test} from 'vitest';
+import OrWhereNotBetween from "../../../../src/builder/statement/where/OrWhereNotBetween.js";
+
+describe('Statement: OrWhereNotBetween', () => {
+    describe('toString', () => {
+        test("It builds where partial statement", () => {
+            const column = 'age';
+            const values = [20, 55];
+            const expectedResult = "age NOT BETWEEN 20 and 55";
+
+            const result = new OrWhereNotBetween(column, values).toString();
+
+            expect(result).toEqual(expectedResult);
+        });
+
+        test("It builds with 'AND' when withSeparator is true", () => {
+            const column = 'age';
+            const values = [20, 55];
+            const expectedResult = "OR age NOT BETWEEN 20 and 55";
+
+            const result = new OrWhereNotBetween(column, values).toString(true);
+
+            expect(result).toEqual(expectedResult);
+        });
+    });
+});
