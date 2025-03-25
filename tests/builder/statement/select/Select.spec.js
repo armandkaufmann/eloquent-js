028349684a801204c4c198faef49b9393dcbf5df
Armand Kaufmann <armandkaufmann@gmail.com>
2025-03-24 20:55:45 -0400

    backfill tests

--- tests/builder/statement/select/Select.spec.js
+++ tests/builder/statement/select/Select.spec.js
@@ -1,9 +1,9 @@
 import {describe, expect, test} from 'vitest';
 import Select from "../../../../src/builder/statement/select/Select.js";
 
-describe('Statement: Where', () => {
+describe('Statement: Select', () => {
     describe('toString', () => {
-        test("It builds where partial statement", () => {
+        test("It builds select partial statement", () => {
             const columns = ['name', 'age', 'sex'];
             const expectedResult = "name, age, sex";
 
@@ -19,6 +19,28 @@ describe('Statement: Where', () => {
             const result = new Select(columns).toString(true);
 
             expect(result).toEqual(expectedResult);
+        });
+    });
+
+    describe('Prepare', () => {
+        test("It builds prepare object select partial statement", () => {
+            const columns = ['name', 'age', 'sex'];
+            const expectedResult = "name, age, sex";
+
+            const result = new Select(columns).prepare();
+
+            expect(result.query).toEqual(expectedResult);
+            expect(result.bindings).toEqual([]);
+        });
+
+        test("It does not build prepare object with separator when withSeparator is true", () => {
+            const columns = ['name', 'age', 'sex'];
+            const expectedResult = "name, age, sex";
+
+            const result = new Select(columns).prepare(true);
+
+            expect(result.query).toEqual(expectedResult);
+            expect(result.bindings).toEqual([]);
         });
     });
 });
