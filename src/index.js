d22e94a16e976a09efcc1cf39ae8098ec2cbc4a1
Armand Kaufmann <armandkaufmann@gmail.com>
2025-11-11 14:45:38 -0500

    update package.json and re-created package-lock.json
    - added main entry point for package.json => src/index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export { DB } from "./DB.js";
+export { Model } from "./Model.js";
+export { Query } from "./builder/Query.js";
+export { config } from "./config/Config.js";
+export { defaultConfig } from "./config/Default.js";
+export * as DBErrors from "./errors/DB/Errors.js";
+export * as QueryBuilderErrors from "./errors/QueryBuilder/Errors.js";
