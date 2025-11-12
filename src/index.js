00332a8c702dca0ef585d67175433fe65f1fe45f
Armand Kaufmann <72105874+armandkaufmann@users.noreply.github.com>
2025-11-12 09:37:24 -0500

    Minor updates nov 12 (#14)
    * 1.0.4
    
    * update readme
    
    * remove models from being accessible via the api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { DB } from "./DB.js";
-export { Model } from "./Model.js";
+// export { Model } from "./Model.js";
 export { Query } from "./builder/Query.js";
 export { config } from "./config/Config.js";
 export { defaultConfig } from "./config/Default.js";
